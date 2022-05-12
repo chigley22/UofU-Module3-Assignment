8d7c21f28697ff3c80da84e135a6747a89cb2029
chigley22 <102197101+chigley22@users.noreply.github.com>
2022-05-11 20:52:51 -0600

    added in push.apply for arrays

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var lettersChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
 
-var capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
+var capLettersChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
 
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbersChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var special = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~",];
+var specialChar = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~",];
+var possibleChar = []
 
 
 
 function generatePassword() {
   var passwordLength = window.prompt("Please enter Password length. Atleast 8, no more than 128 characters.");
+  
+  possibleChar.push.apply(possibleChar, lettersChar)
+
   if (passwordLength <= 8 || passwordLength >= 128){
     generatePassword();
     return;
@@ -22,27 +26,25 @@ function generatePassword() {
 
   if (special == "Yes"){
 
-  } else {
-
+    possibleChar.push.apply(possibleChar, specialChar)
   }
-
   var numbersOkay = window.prompt("Would you like to include numbers?")
   numbersOkay = numbersOkay.toUpperCase();
 
-  if (numbersOkay == "Yes"){ 
+  if (numbersOkay == "Yes"){
 
-  } else {
-
+  possibleChar.push.apply(possibleChar, numbersChar)
   }
-
   var capLetters = window.prompt("Would you like to include Capital Letters?")
   capLetters = capLetters.toUpperCase();
 
-  if (capLetters == "Yes") {
+  if (capLetters == "Yes"){
 
-  } else {
-
+  possibleChar.push.apply(possibleChar, capLettersChar)
   }
+  console.log(possibleChar)
+
+  var newPass = []
 }
 
 // Write password to the #password input
