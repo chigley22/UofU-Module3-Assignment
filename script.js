127825b4396527ce5f4f477b60b9bdfee3a6c747
chigley22 <102197101+chigley22@users.noreply.github.com>
2022-09-16 01:54:27 -0600

    script.js update

--- script.js
+++ script.js
@@ -1,60 +1,147 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lettersChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var lettersChar = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "x",
+  "y",
+  "z",
+];
 
-var capLettersChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
+var capLettersChar = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "X",
+  "Y",
+  "Z",
+];
 
 var numbersChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var specialChar = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~",];
-var possibleChar = []
-
-
+var specialChar = [
+  " ",
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+var possibleChar = [];
 
 function generatePassword() {
-  var passwordLength = window.prompt("Please enter Password length. Atleast 8, no more than 128 characters.");
-  
-  possibleChar.push.apply(possibleChar, lettersChar)
+  var passwordLength = window.prompt(
+    "Please enter Password length. Atleast 8, no more than 128 characters."
+  );
 
-  if (passwordLength <= 8 || passwordLength >= 128){
+  possibleChar.push.apply(possibleChar, lettersChar);
+
+  if (passwordLength <= 8 || passwordLength >= 128) {
     generatePassword();
     return;
   }
-  var special = window.prompt("Would you like to include special characters?")
+  var special = window.prompt("Would you like to include special characters?");
   special = special.toUpperCase();
 
-  if (special == "Yes"){
-
-    possibleChar.push.apply(possibleChar, specialChar)
+  if (special == "Yes") {
+    possibleChar.push.apply(possibleChar, specialChar);
   }
-  var numbersOkay = window.prompt("Would you like to include numbers?")
+  var numbersOkay = window.prompt("Would you like to include numbers?");
   numbersOkay = numbersOkay.toUpperCase();
 
-  if (numbersOkay == "Yes"){
-
-  possibleChar.push.apply(possibleChar, numbersChar)
+  if (numbersOkay == "Yes") {
+    possibleChar.push.apply(possibleChar, numbersChar);
   }
-  var capLetters = window.prompt("Would you like to include Capital Letters?")
+  var capLetters = window.prompt("Would you like to include Capital Letters?");
   capLetters = capLetters.toUpperCase();
 
-  if (capLetters == "Yes"){
-
-  possibleChar.push.apply(possibleChar, capLettersChar)
+  if (capLetters == "Yes") {
+    possibleChar.push.apply(possibleChar, capLettersChar);
   }
-  console.log(possibleChar)
+  // console.log(possibleChar);
+  var password = "";
+  for (i = 0; i <= length; i++) {
+    var randomNum = Math.floor(Math.random() * possibleChar.length);
+    password += possibleChar[randomNum];
+  }
 
-  var newPass = []
+  return password;
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  passwordText.value - " ";
+  var password = generatePassword();
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
+var generateBtn = document.querySelector("#generate");
+
 generateBtn.addEventListener("click", writePassword);
